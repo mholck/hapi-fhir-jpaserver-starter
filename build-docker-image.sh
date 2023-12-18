aa73dd36d4a00c90536aa6568bfa0fb255b0371a
Michael Holck <michael.holck@icf.com>
2023-12-18 08:08:53 -0700

    Setup for new fhir.ecqm.icfcloud.com server

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker build -t hapi-fhir/hapi-fhir-jpaserver-starter .
-
+docker buildx build --platform=linux/amd64 -t icf/hapi-server .
+docker tag icf/hapi-server:latest 531166169809.dkr.ecr.us-east-1.amazonaws.com/hapi_fhir:latest
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 531166169809.dkr.ecr.us-east-1.amazonaws.com
+docker push 531166169809.dkr.ecr.us-east-1.amazonaws.com/hapi_fhir:latest
